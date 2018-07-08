c1b4acb16c752bde93116b87936b6563ec0d60f8
unknown <dzmitry.bashlak@itechart-group.com>
2018-07-08 17:16:10 +0300

    purge

--- objects/BaseAppliance.js
+++
@@ -1,7 +0,0 @@
-class BaseAppliance {
-    constructor(powerConsumption, pluggedInState) {
-        this.powerConsumption = powerConsumption;
-        this.pluggedInState = pluggedInState !== 'false' ? true : false;
-    }
-};
-module.exports = BaseAppliance;
